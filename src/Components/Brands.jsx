1054cb476531d2ba9f6891d9929b0f70bc125c86
BHARATHIS1221 <bhar21ec022@rmkcet.ac.in>
2026-07-23 22:53:31 +0530

    Fixed image imports for Netlify

--- src/Components/Brands.jsx
+++ src/Components/Brands.jsx
@@ -1,49 +1,62 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// Import Images
+import banner from "../assets/banner.jpeg";
+import img1 from "../assets/img1.jpeg";
+import img2 from "../assets/img2.jpeg";
+import img3 from "../assets/img3.jpeg";
+import img4 from "../assets/img4.jpeg";
+import img5 from "../assets/img5.jpeg";
+import img6 from "../assets/img6.jpeg";
+import img7 from "../assets/img7.jpeg";
+import img8 from "../assets/img8.jpeg";
+import img9 from "../assets/img9.jpeg";
+import img10 from "../assets/img10.jpeg";
+
 const brands = [
   {
-    img: "src/assets/banner.jpeg",
+    img: banner,
     text: "New SPF 50 Safe For Sensitive Skin",
   },
   {
-    img: "src/assets/img1.jpeg",
+    img: img1,
     text: "Get a Beauty Blender & Gloss",
   },
   {
-    img: "src/assets/img2.jpeg",
+    img: img2,
     text: "#1 Hydration Duo",
   },
   {
-    img: "src/assets/img3.jpeg",
+    img: img3,
     text: "New Collection. New Rules",
   },
   {
-    img: "src/assets/img4.jpeg",
+    img: img4,
     text: "Flat 15% Off on Your 1st Order",
   },
   {
-    img: "src/assets/img5.jpeg",
+    img: img5,
     text: "Up to 40% Off",
   },
   {
-    img: "src/assets/img6.jpeg",
+    img: img6,
     text: "Glow Routine Flawless Look",
   },
   {
-    img: "src/assets/img7.jpeg",
+    img: img7,
     text: "Bestseller Gifts on Orders",
   },
   {
-    img: "src/assets/img8.jpeg",
+    img: img8,
     text: "Scents That Embody Luxury",
   },
   {
-    img: "src/assets/img9.jpeg",
+    img: img9,
     text: "Get the Dior Glow",
   },
   {
-    img: "src/assets/img10.jpeg",
+    img: img10,
     text: "Purified Peptides",
   },
 ];
@@ -51,22 +64,20 @@ const brands = [
 const Brands = () => {
   return (
     <div className="brands-container">
-      {/* Full screen banner */}
+      {/* Banner */}
       <img
         src={brands[0].img}
         alt="banner"
-        style={{
-          width: "100%",
-        }}
+        style={{ width: "100%" }}
       />
 
-      {/* Grid for remaining images */}
+      {/* Grid */}
       <div className="brands-grid">
         {brands.slice(1).map((brand, index) => (
           <div className="brand-card" key={index}>
             <img
               src={brand.img}
-              alt="brand"
+              alt={brand.text}
               style={{
                 width: "100%",
                 height: "200px",
@@ -79,8 +90,11 @@ const Brands = () => {
         ))}
       </div>
 
-        
-      <Link  to="/newnykaa" className="btn"><button className="explore-btn">Explore All Brands</button> </Link>
+      <Link to="/newnykaa" className="btn">
+        <button className="explore-btn">
+          Explore All Brands
+        </button>
+      </Link>
     </div>
   );
 };
